7ea463c427591dd6c306a3a882f15a3256daf399
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-09-11 17:36:14 +0500

    changes

--- src/context.js
+++ src/context.js
@@ -1,12 +1,10 @@
-
+import React from "preact/compat";
 
 export const AppContext = React.createContext();
 
-const AppProvider =({children})=>{
-    return(
-        <AppContext.Provider value={"Website"}>
-            {children}
-        </AppContext.Provider>
-    )
-}
+const AppProvider = ({ children }) => {
+  return (
+    <AppContext.Provider value={"Website"}>{children}</AppContext.Provider>
+  );
+};
 export default AppProvider;
