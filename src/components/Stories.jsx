3926c59363796926072723e8c20f6fe8941f7227
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-09-11 15:44:32 +0500

    changes

--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -1,20 +1,18 @@
-import { useEffect } from "preact/hooks";
-import React from "react";
+import React, { useEffect } from "react";  // Correct import for React hooks
 
 export default function Stories() {
   let API = "https://hn.algolia.com/api/v1/search?query=html";
-
+  
   async function fetchApi(url) {
-    try{
-        let response = await fetch(url);
-        let data = await response.json();
-        console.log(data);
-    }
-    catch (error){
-      console.log(error);
-      
+    try {
+      let response = await fetch(url);
+      let data = await response.json();
+      console.log("Fetched data: ", data);  // Log fetched data
+    } catch (error) {
+      console.log("Error fetching data: ", error);
     }
   }
+  
 
   useEffect(() => {
     fetchApi(API);
