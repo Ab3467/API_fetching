4936d87af2792a4172f886b6413896e8e37817de
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-09-11 15:52:01 +0500

    changes

--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -4,10 +4,11 @@ export default function Stories() {
   let API = "https://hn.algolia.com/api/v1/search?query=html";
 
   async function fetchApi(url) {
+    console.log("Fetching API...");
     try {
       let response = await fetch(url);
       let data = await response.json();
-      console.log("Fetched data: ", data); // Log fetched data
+      console.log("Fetched data: ", data);
     } catch (error) {
       console.log("Error fetching data: ", error);
     }
