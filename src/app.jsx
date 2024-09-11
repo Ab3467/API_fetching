d9079b4a82e187909e155cb96ade206dfe4752fa
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-09-11 17:07:38 +0500

    changes

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,15 @@
-import React from "preact/compat";
+import React, { useContext } from "preact/compat";
 import Stories from "./components/Stories";
-
+import { AppContext } from "./context";
 
 export default function App() {
+  const data = useContext(AppContext);
+
   return (
     <div>
       <p>Welcome to website</p>
       <h2>Basic Content Test</h2> {/* Simple output to check rendering */}
-      <Stories/>
+      <Stories />
     </div>
   );
 }
